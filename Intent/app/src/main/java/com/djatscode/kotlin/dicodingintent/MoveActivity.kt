a18e3c274e1612a4ed6dac30ef60ff90cf105110
AryoDjat1 <aryodjati5@gmail.com>
2022-11-09 20:46:05 +0700

    finished intent practice

---
+++ Intent/app/src/main/java/com/djatscode/kotlin/dicodingintent/MoveActivity.kt
@@ -0,0 +1,13 @@
+package com.djatscode.kotlin.dicodingintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+
+
+    }
+}
