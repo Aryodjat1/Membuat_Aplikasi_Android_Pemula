a247039e088e2fe47f8912aa52c37ad6da5f5a70
AryoDjat1 <aryodjati5@gmail.com>
2022-11-21 11:15:54 +0700

    repairing code and add splashscreen

--- Submission/app/src/main/java/com/djatscode/kotlin/dicodingsubmission/DetailPage.kt
+++ Submission/app/src/main/java/com/djatscode/kotlin/dicodingsubmission/DetailPage.kt
@@ -2,10 +2,36 @@ package com.djatscode.kotlin.dicodingsubmission
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class DetailPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_page)
+
+        val intent: Bundle? = intent.extras
+        val aTitle = intent?.getString("title")
+        val aDesc = intent?.getString("desc")
+        val aGenre = intent?.getString("genre")
+        val aRating = intent?.getDouble("rating", 0.0)
+        val aPhoto = intent?.getInt("photo", 0)
+
+        val dtlPhoto: ImageView = findViewById(R.id.img_dtl_photo)
+        val dtlTitle: TextView = findViewById(R.id.tv_dtl_title)
+        val dtlGenre: TextView = findViewById(R.id.tv_dtl_genre)
+        val dtlRate: TextView = findViewById(R.id.tv_dtl_rating)
+        val dtlDesc: TextView = findViewById(R.id.tv_dtl_desc)
+
+        dtlTitle.text = aTitle
+        dtlGenre.text = aGenre
+        dtlRate.text = aRating.toString()
+        dtlDesc.text = aDesc
+        Glide.with(this)
+            .load(aPhoto)
+            .apply(RequestOptions().override(55, 55))
+            .into(dtlPhoto)
     }
 }
--- Submission/app/src/main/java/com/djatscode/kotlin/dicodingsubmission/Splashscreen.kt
+++ Submission/app/src/main/java/com/djatscode/kotlin/dicodingsubmission/Splashscreen.kt
@@ -1,11 +1,24 @@
 package com.djatscode.kotlin.dicodingsubmission
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        supportActionBar?.hide()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(5000)
+            startActivity(Intent(this@Splashscreen, MainActivity::class.java))
+            finish()
+        }
     }
 }
